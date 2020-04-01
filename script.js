9c56e51e43fa743444baab1dacb1c58d2849933a
Alex Travick <alextravick18@gmail.com>
2020-04-01 13:00:16 -0400

    added bricks into canvas

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const ctx = canvas.getContext('2d');
 let score = 0;
 
 
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 
 //create ball props
 const ball = {
@@ -18,6 +21,8 @@ const ball = {
   dx: 4,
   dy: -4
 }
+
+
 
 // Draw ball on canvas
 function drawBall() {
@@ -38,6 +43,30 @@ const paddle = {
   dx: 0
 }
 
+
+// Create brick props
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true
+}
+
+// Create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x,y, ...brickInfo }
+  }
+}
+
+console.log(bricks);
+
 // Draw a paddle on canvas
 function drawPaddle() {
   ctx.beginPath();
@@ -47,21 +76,41 @@ function drawPaddle() {
   ctx.closePath();
 }
 
-// Draw items
-function draw() {
-  drawBall();
-  drawPaddle();
-  drawScore();
-}
-
-draw();
-
 
 // Draw score on canvas
 function drawScore() {
   ctx.font = '20px Lato';
   ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
 }
+
+
+// Draw bricks on canvas
+function drawBricks() {
+  bricks.forEach(column => {
+    column.forEach(brick => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? '0095dd' : 'transparent';
+      ctx.fill();
+      ctx.closePath();
+    })
+  })
+}
+
+
+
+
+// Draw items
+function draw() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawBricks();
+}
+
+draw();
+
+
 
 
 // Event listeners
