a35b85fd7621ae678785048a1ecd876c9a505730
Alex Travick <alextravick18@gmail.com>
2020-04-01 11:43:02 -0400

    added ball, paddle & score

--- script.js
+++ script.js
@@ -1,11 +1,67 @@
 const rules = document.getElementById('rules');
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+let score = 0;
 
 
 
+//create ball props
+const ball = {
+  x: canvas.width/2,
+  y: canvas.height/2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4
+}
+
+// Draw ball on canvas
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
+// Create paddle props
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0
+}
+
+// Draw a paddle on canvas
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
+// Draw items
+function draw() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+}
+
+draw();
 
 
+// Draw score on canvas
+function drawScore() {
+  ctx.font = '20px Lato';
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
+}
 
 
 // Event listeners
@@ -15,4 +71,4 @@ rulesBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
   rules.classList.remove('show');
-})
+}) 
