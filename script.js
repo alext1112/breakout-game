bc102fe91bcc9877d54a008273b453c585463f86
Alex Travick <alextravick18@gmail.com>
2020-04-01 10:52:25 -0400

    added css styling, rules transitions

---
+++ script.js
@@ -0,0 +1,18 @@
+const rules = document.getElementById('rules');
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+
+
+
+
+
+
+
+// Event listeners
+rulesBtn.addEventListener('click', () => {
+  rules.classList.add('show');
+})
+
+closeBtn.addEventListener('click', () => {
+  rules.classList.remove('show');
+})
